075b594ea599aad5c1be0db1a494e9ae9dbb2e3e
casio <carsten.kraus@gmail.com>
2017-07-05 11:22:49 +0200

    redux connected

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
+import {switchFoo} from '../store';
 import logo from '../assets/logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const {foo} = this.props
+    const {switchFoo} = this.props
+    
     return (
       <div className="App">
         <div className="App-header">
+          <h1>{foo}!</h1>
+          <button onClick={switchFoo}>Do it!</button>
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
@@ -18,4 +26,22 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    foo: state.foo
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    switchFoo: function() {
+      const action = {type: 'SWITCH_FOO'}
+      dispatch(action)
+    }
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,21 @@
 import {createStore} from 'redux'
 
+const SWITCH_FOO = 'SWITCH_FOO'
+
+export const switchFoo = () => {
+  return {
+    type: SWITCH_FOO
+  }
+}
+
+
 const reducer = (state = {foo: 'bar'}, action) => {
   switch (action.type) {
-    // case value:
-    //   break;
+    case SWITCH_FOO:
+      return {...state, foo: 'EGGZ'}
   
     default:
+      console.log('### reducer', action)
       return state
   }
 }
